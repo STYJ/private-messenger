8cd9d67595c996031586d5f4b72990017d1d6a63
Simon Tan Yu Jing <simon.tan.yu.jing@gmail.com>
2019-08-03 11:44:03 +0800

    deploy

---
+++ precache-manifest.80127fa38bdccdbb412898f75395e025.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "90426e9afcbc0acf3744",
+    "url": "/private-messenger/css/app.7ff47448.css"
+  },
+  {
+    "revision": "90426e9afcbc0acf3744",
+    "url": "/private-messenger/js/app.6ac2f67a.js"
+  },
+  {
+    "revision": "6c74979b8660f739ba33",
+    "url": "/private-messenger/css/chunk-vendors.55fa9b2e.css"
+  },
+  {
+    "revision": "6c74979b8660f739ba33",
+    "url": "/private-messenger/js/chunk-vendors.8114ada3.js"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/private-messenger/fonts/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/private-messenger/fonts/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/private-messenger/fonts/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/private-messenger/fonts/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/private-messenger/fonts/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/private-messenger/fonts/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/private-messenger/fonts/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/private-messenger/fonts/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/private-messenger/fonts/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/private-messenger/fonts/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/private-messenger/fonts/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/private-messenger/fonts/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/private-messenger/img/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/private-messenger/img/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/private-messenger/img/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "59c0ab453168b3ea853ffa865e3b639e",
+    "url": "/private-messenger/img/logo.59c0ab45.png"
+  },
+  {
+    "revision": "1fb51c174c9c7c1c2f21b395482cbe8f",
+    "url": "/private-messenger/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/private-messenger/robots.txt"
+  }
+];
